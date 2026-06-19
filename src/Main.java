d2975cb33abe45401c82473a30320a2b1e8bf298
Tin Kežić <kezic.tin@gmail.com>
2026-06-19 17:19:56 +0200

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,65 @@ import agent.OtaAgent;
 import model.Device;
 import model.UpdatePackage;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Device device =
+        List<Device> canaryDevices = List.of(
                 new Device(
-                        "ESP32-001",
-                        "1.0.0");
+                        "ESP32-CANARY-001",
+                        "1.0.0",
+                        true),
+
+                new Device(
+                        "ESP32-CANARY-002",
+                        "1.0.0",
+                        true)
+        );
+
+        List<Device> productionDevices = List.of(
+                new Device(
+                        "ESP32-101",
+                        "1.0.0",
+                        false),
+
+                new Device(
+                        "ESP32-102",
+                        "1.0.0",
+                        false),
+
+                new Device(
+                        "ESP32-103",
+                        "1.0.0",
+                        false)
+        );
 
         UpdatePackage updatePackage =
-                new UpdatePackage(
-                        "1.1.0");
+                new UpdatePackage("1.1.0");
 
         OtaAgent agent = new OtaAgent();
 
-        agent.execute(
-                device,
+        agent.executeCanary(
+                canaryDevices,
+                productionDevices,
                 updatePackage);
 
-        System.out.println(
-                "Device version: "
-                        + device.getCurrentVersion()
-        );
+        System.out.println("\n=== Final Versions ===");
+
+        canaryDevices.forEach(device ->
+                System.out.println(
+                        device.getId()
+                                + " -> "
+                                + device.getCurrentVersion()
+                ));
+
+        productionDevices.forEach(device ->
+                System.out.println(
+                        device.getId()
+                                + " -> "
+                                + device.getCurrentVersion()
+                ));
     }
 }
