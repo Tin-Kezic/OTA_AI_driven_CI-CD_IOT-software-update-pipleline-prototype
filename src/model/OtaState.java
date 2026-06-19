6b1c781d3289730975ab45c571b001118020cf22
Tin Kežić <kezic.tin@gmail.com>
2026-06-19 17:10:10 +0200

    added states

--- src/model/OtaState.java
+++ src/model/OtaState.java
@@ -3,6 +3,12 @@ package model;
 public enum OtaState {
     PREPARED,
     VALIDATED,
+
+    CANARY_DEPLOYED,
+    CANARY_EVALUATION,
+
+    FULL_ROLLOUT,
+
     DEPLOYED,
     ACTIVATED,
     FAILED
