a95e32ef3c5bd6329d63e7f4b963a704c33a43b3
Tin Kežić <kezic.tin@gmail.com>
2026-06-19 17:20:36 +0200

    modified TelemetryService.java

---
+++ src/telemetry/TelemetryService.java
@@ -0,0 +1,14 @@
+package telemetry;
+
+public class TelemetryService {
+
+    public double getSuccessRate() {
+        // simulacija
+        return 98.5;
+    }
+
+    public int getCrashCount() {
+
+        return 2;
+    }
+}
