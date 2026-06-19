3fcd8eb4bbc51ba126733fb3beace3569ad0ea21
Tin Kežić <kezic.tin@gmail.com>
2026-06-19 17:20:04 +0200

    modified Device.java

--- src/model/Device.java
+++ src/model/Device.java
@@ -1,24 +1,28 @@
 package model;
 
 public class Device {
-
     private String id;
     private String currentVersion;
+    private boolean canary;
 
-    public Device(String id, String currentVersion) {
+    public Device(String id,
+                  String currentVersion,
+                  boolean canary) {
+
         this.id = id;
         this.currentVersion = currentVersion;
+        this.canary = canary;
     }
-
     public String getId() {
         return id;
     }
-
     public String getCurrentVersion() {
         return currentVersion;
     }
-
     public void setCurrentVersion(String currentVersion) {
         this.currentVersion = currentVersion;
+    }
+    public boolean isCanary() {
+        return canary;
     }
 }
